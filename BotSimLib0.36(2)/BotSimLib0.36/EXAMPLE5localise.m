59089399085e5d100fac500d9e67c479e60933f5
f-rower <fran_gt6@hotmail.com>
2017-10-22 18:10:19 +0100

    Initial localisation ready. Now I just need to play around with the numbers for convergence area in different maps, number of particles in different maps, how much I move in case of not convergence and the cumulative probability needed for convergence.

--- BotSimLib0.36(2)/BotSimLib0.36/EXAMPLE5localise.m
+++ BotSimLib0.36(2)/BotSimLib0.36/EXAMPLE5localise.m
@@ -1,9 +1,11 @@
 clf;        %clears figures
 clc;        %clears console
-clear;      %clears workspace
+clear all; 
+close all;%clears workspace
 axis equal; %keeps the x and y scale the same
-map=[0,0;60,0;60,45;45,45;45,59;106,59;106,105;0,105];  %default map
-
+%map=[0,0;60,0;60,45;45,45;45,59;106,59;106,105;0,105];  %default map
+map = [0,0;60,0;60,50;100,50;70,0;110,0;150,80;30,80;30,40;0,80];
+%map = [-30,0;-30,40;30,40;30,60;5,60;45,90;85,60;60,60;60,40;120,40;120,60;95,60;135,90;175,60;150,60;150,40;210,40;210,60;185,60;225,90;265,60;240,60;240,40;300,40;300,0]; %repeated features
 
 
 %% Robot setup
@@ -11,15 +13,15 @@ map=[0,0;60,0;60,45;45,45;45,59;106,59;106,105;0,105];  %default map
 botSim = BotSim(map,[0,0,0]);  %sets up a botSim object a map, and debug mode on.
 botSim.drawMap();
 drawnow;
-%botSim.randomPose(10); %puts the robot in a random position at least 10cm away from a wall
-botSim.setBotPos([20,15]);
-botSim.setBotAng(pi/3);
+botSim.randomPose(5); %puts the robot in a random position at least 10cm away from a wall
+%botSim.setBotPos([50,80]);
+%botSim.setBotAng(0);
 target = botSim.getRndPtInMap(10);  %gets random target.
 
 tic %starts timer
 
 %% your localisation function is called here.
-[returnedBot,botScan,partData,partLoc,specPart] = localise(botSim,map,target); %Where the magic happens
+[returnedBot,botScan,partData,partLoc,specPart,orientVect] = localise(botSim,map,target); %Where the magic happens
 
 resultsTime = toc %stops timer
 
